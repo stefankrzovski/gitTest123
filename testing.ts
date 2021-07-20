29917fbe9a0e6cae462bf7c43c21e6a5964df772
stefankrzovski <stefan.krzovski@yahoo.com>
2021-07-20 15:58:56 +0200

    somethingdsa

--- testing.ts
+++ testing.ts
@@ -6,3 +6,15 @@ const chalk = require("chalk")
 
 
 console.log(chalk.red("Something"))
+
+childProcess.execSync(
+    "gh pr list"
+)
+
+// childProcess.execSync(
+//     "gh pr checkout loka",
+// )
+//
+// childProcess.execSync(
+//     "gh pr create --base devBuild --head loka --title 'Version CodePush' ",
+// )
