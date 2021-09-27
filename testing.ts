2e3a26bfe0504dc2679404481254579d5ad4b751
stefankrzovski <stefan.krzovski@yahoo.com>
2021-09-27 18:50:33 +0200

    whatever

--- testing.ts
+++ testing.ts
@@ -7,10 +7,25 @@ const chalk = require("chalk")
 
 console.log(chalk.red("Something"))
 
-childProcess.execSync(
-    "gh pr checkout loka",
-)
+// childProcess.execSync(
+//     "gh pr checkout loka",
+// )
+// //
+// childProcess.execSync(
+//     "gh pr create --base origin/devBuild --head origin/loka --title 'Yata' ",
+// )
+
+childProcess.execSync("gh pr checkout loka")
+childProcess.execSync("gh pr create --base origin/devBuild --head origin/loka --title \"CodePush version\"")
+childProcess.execSync("gh pr merge")
+
 //
-childProcess.execSync(
-    "gh pr create --base origin/devBuild --head origin/loka --title 'Yata' ",
-)
+// gh pr checkout loka
+//
+// gh pr create --base devBuild --head loka --title "CodePush version"
+//
+// const result = spawn.sync("gh pr list --json number", { encoding : 'UTF-8' })
+//
+// const number = JSON.parse(result.stdout)[0].number
+//
+// gh pr merge ${number}
